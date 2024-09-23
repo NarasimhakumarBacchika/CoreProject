00e22f05320b24fdd89a6992241dd06fb3853c6d
Narasimhakumarbacchika <narasimhakumarbacchika21@gmail.com>
2024-09-23 22:11:30 +0530

    sample

---
+++ RepositoryPattern1/Model/Registraction.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace RepositoryPattern1.Model
+{
+    [Keyless]
+    public partial class Registraction
+    {
+        public int Id { get; set; }
+        [StringLength(50)]
+        [Unicode(false)]
+        public string? UserName { get; set; }
+        [StringLength(50)]
+        [Unicode(false)]
+        public string? Password { get; set; }
+        [StringLength(50)]
+        [Unicode(false)]
+        public string? RetypePassword { get; set; }
+        public int? EmpId { get; set; }
+        [Column(TypeName = "decimal(18, 0)")]
+        public decimal? Mobile { get; set; }
+        [StringLength(50)]
+        [Unicode(false)]
+        public string? Name { get; set; }
+        public bool? IsActive { get; set; }
+    }
+}
